42a44002bcde5109b308d64e498c6ea3ee20671b
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-27 15:04:37 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,10 +18,8 @@ class Artist
   end
   
   def add_song_by_name(name)
-    #song = Song.new(name)
-    #add_song(song)
-    @songs.push(name)
-    name.artist = self
+    song = Song.new(name)
+    add_song(song)
     @@song_count += 1
   end
   
