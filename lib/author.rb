028d0e19b62f92b203861bed5fecf3ea387c54ba
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-27 15:29:20 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,8 +1,6 @@
 class Author 
   
   attr_accessor :name
-  
-  @@post_count = 0
 
   def initialize(name)
     @name = name
@@ -16,18 +14,16 @@ class Author
   def add_post(display)
     @posts.push(display)
     display.author = self
-    @@post_count += 1
   end
 
   def add_post_by_title(post_title)
     display = Post.new(post_title)
     @posts.push(display)
     display.author = self
-    @@post_count += 1
   end
 
   def self.post_count
-    @@post_count
+    return Post.all.count
   end
   
 end
