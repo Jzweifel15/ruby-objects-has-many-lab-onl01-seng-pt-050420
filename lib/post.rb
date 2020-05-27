4b57e905cf93146380280cf6abf581eea4722a24
Justin R. Zweifel <jzweifel15@yahoo.com>
2020-05-27 14:56:40 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,10 +1,11 @@
 class Post 
 
-  attr_accessor :name, :author
+  attr_accessor :name, :author, :title
   
   @@all = []
   
   def initialize(title)
+    @title = title
     @name = name
     @author = author
     save
